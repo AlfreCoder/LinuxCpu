7099f5585bc88f5d78b61aae74fbef7b3a7770c2
Alfredo Gonzalez Calvin <ojopython@gmail.com>
2018-04-09 17:06:48 +0000

    Update frecucore.py

--- frecucore.py
+++ frecucore.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-'''Medicion de la frecuencia por segundo de la cpu, usarlo como SUDO'''
+'''Measure of a dualcore cpu frecuency, requires SUDO privileges'''
 import os
 from time import sleep
 import sys
-print("-----^C to exit-----")
-print("Recuerda que se pueden ejecutar globalmente.....")
+print("-----^C to exit-----\n")
 medida = 0
 while True:
         try:
@@ -12,14 +11,14 @@ while True:
                      as f, open("/sys/devices/system/cpu/cpu1/cpufreq/cpuinfo_cur_freq") as g:
                         core0 = f.read()
                         core1 = g.read()
-                with open("/sys/devices/system/cpu/cpu2/cpufreq/cpuinfo_cur_freq") as h, \
-                     open("/sys/devices/system/cpu/cpu3/cpufreq/cpuinfo_cur_freq") as i:
-                        core2 = h.read()
-                        core3 = i.read()
+                        cores = [core0,core1]
                         medida += 1
-                        print("########--Measure number {}--#################\n".format(medida))
-                        print("      #CPU CORE1 FREQ %s      #CPU CORE2 FREQ %s      #CPU CORE3 FREQ %s      #CPU CORE4 FREQ %s" %(core0,core1,core2,core3))
-                        sleep(1.0)
+                        print("<><><><><>--Measure number {}--<><><><><><>\n".format(medida))
+                        for i in range(0,len(cores)):
+                                print("> Core{} CPU frequency in Mhz: {}".format(i+ 1, cores[i]))
+                        print("<><><><><><><><><><><><><><><><><><><><><>\n\n\n")
+                        sleep(1.5)
+                        
         except:
                 if os.getuid() != 0:
                     print("This script requires sudo privileges, Quitting...")
