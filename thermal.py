c02d8f410096e5b8a15b95a503c96a3de68c7eb3
Alfredo Gonzalez Calvin <ojopython@gmail.com>
2018-04-09 21:39:10 +0000

    Update thermal.py

--- thermal.py
+++ thermal.py
@@ -5,7 +5,7 @@ print("----------^C to exit----------")
 while True:
         try:
                 with open("/sys/class/thermal/thermal_zone0/temp") as f:
-                    print("Cpu Temperature {}-C".format(int(f.read())/1000))
+                    print("Cpu Temperature {}'C".format(int(f.read())/1000))
                     sleep(1.0)
         except:
                 print("Quitting..")
